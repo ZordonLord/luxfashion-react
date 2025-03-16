c27b01f5c2a5ceb46c47707922a9f28928646e44
ZordonLord <zordonlord@mail.ru>
2025-03-16 16:03:07 +1000

    add luxfashion React

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Message from './components/Message';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Message text="Привет, это мой первый компонент Message!" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
